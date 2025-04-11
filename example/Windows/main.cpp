e3a786c9e572b5fae812bd5c01685d942edc2fda
minsoo.song <minsoo.song@doosan.com>
2025-04-11 18:39:31 +0900

    bugfix window_main.cpp

--- example/Windows/main.cpp
+++ example/Windows/main.cpp
@@ -10,32 +10,13 @@
 #include <iostream>
 #include <cstring>
 
-//typedef struct _SERIAL_BODY_HEAD {
-//    /* serial sequnece number */
-//    unsigned short              _iSeqNo;
-//    /* serial command id */
-//    unsigned short              _iCmdId;
-//} SERIAL_BODY_HEAD;
-//
-//int main(){
-//    SERIAL_BODY_HEAD tHead = { 0, 1758, };
-//    std::cout << "HEADER - tHead._iSeqNo : " << tHead._iSeqNo << ", tHead._iCmdId : " << tHead._iCmdId << std::endl;
-//    int nTotalLength = 0;
-//    char szPacket[8192] = {'\0', };
-//    // ��� ���
-//    memcpy(szPacket + nTotalLength, &tHead, sizeof(SERIAL_BODY_HEAD));
-//    nTotalLength += sizeof(SERIAL_BODY_HEAD);
-//    std::cout << "111PACKET CONTENTS " << std::endl;
-//    for(int i=0;i<nTotalLength; i++){
-//        std::cout << "szPacket[" << i << "]: " << (int) szPacket[i] << std::endl;
-//    }
-//    return 0;
-//}
+
 
 //
 // Specify drfl header.
 #include "../../include/DRFLEx.h"
 #include "util.hpp"
+
 
 const std::string IP_ADDRESS = "192.168.137.100";
 
@@ -145,12 +126,9 @@ void OnMonitoringDataExCB(const LPMONITORING_DATA_EX pData) {
   // cout << "OnMonitoringDataExCB"<<std::endl;
 }
 
-void OnMonitoringDataExCB2(const LPMONITORING_CTRLIO_EX2 pData) {
+void OnMonitoringCrtlIoExCB(const LPMONITORING_CTRLIO_EX pData) {
   // cout << "OnMonitoringDataExCB"<<std::endl;
 }
-
-
-
 /*
 1. move 연속으로 했을 때 수행되지 않음. 
 2. mwait 모션 수행하지 않음 -> amovej 이동 및 mwait 이용.
@@ -179,11 +157,11 @@ int main() {
   // robot.set_on_tp_initializing_completed(OnTpInitializingCompleted);
 
   // Registered empty callbacks (drfl down if received none for 3 sec.)
-  robot.set_on_monitoring_data_ex(OnMonitoringDataExCB);
   
+  robot.set_on_monitoring_data_ex(OnMonitoringDataExCB);
   robot.set_on_monitoring_state(OnMonitoringStateCB);
   robot.set_on_log_alarm(OnLogAlarm);
-  robot.set_on_monitoring_ctrl_io_ex2(OnMonitoringDataExCB2);
+  robot.set_on_monitoring_ctrl_io_ex(OnMonitoringCrtlIoExCB);
   // robot.set_auto_safety_move_stop(true);
   
   
@@ -199,13 +177,8 @@ int main() {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
   std::cout << "Control Granted ! " << std::endl;
+  robot.set_robot_mode(ROBOT_MODE_MANUAL);
 
-  if (true != robot.reset_sequence(0)) { // if we make sure turning on servo on, should be exist.
-    std::cout << "reset_sequence failure " << std::endl;
-    return 1;
-  }
-  std::cout << "reset_sequence done" <<std::endl;
-  
   ROBOT_STATE robot_state = robot.GetRobotState();
   std::cout << "Robot State : " << to_str(robot_state);
 
@@ -216,137 +189,79 @@ int main() {
     }
     std::cout << "SetRobotControl(Servo On) done " << std::endl;
   }
-
-
+  robot.set_robot_mode(ROBOT_MODE_AUTONOMOUS);
 
 
   while(true){
-    std::cout << "READY TO INPUT " << std::endl;
+    std::cout << "READY TO INPUT : " << std::endl;
     char input;
     cin >> input;
     switch(input){
       case '1':
       {
-        POSITION_EX tTargetPos;
-        float pos[6] = {0.,0.,0.,0.,0.,0.};
-        memcpy(tTargetPos._posj._pos, pos, sizeof(float)*6);
-        tTargetPos._pos_type = POSJ;
-        float fTargetVel[NUMBER_OF_JOINT] = {30., 30., 30., 30., 30., 30.};
-        float fTargetAcc[NUMBER_OF_JOINT] = {30., 30., 30., 30., 30., 30.};
-        if(true != robot.movej(&tTargetPos, fTargetVel, fTargetAcc, 0.f, 0, MOVE_MODE_ABSOLUTE, 0.f, 0 )) {
-          std::cout << "Cannot set movej " << std::endl;
-          break;
-        }
-        std::cout << "Set movej done111" <<std::endl;
+        float pos[6] = {0., 0., 0., 0., 0.,0.};
+        float fTargetVel = 30.0;
+        float fTargetAcc = 30.0;
+        robot.movej(pos, fTargetVel, fTargetAcc);
 
-
-        float pos2[6] = {0.,0.,30.,0.,30.,0.};
-        memcpy(tTargetPos._posj._pos, pos2, sizeof(float)*6);
-        if(true != robot.movej(&tTargetPos, fTargetVel, fTargetAcc, 0.f, 0, MOVE_MODE_ABSOLUTE, 0.f, 0 )) {
-          std::cout << "Cannot set movej " << std::endl;
-          break;
-        }
-        std::cout << "Set movej done222" <<std::endl;
+        float pos2[6] = { 0., 0., 90., 0., 90.,0. };
+        float fTargetVel2 = 30.0;
+        float fTargetAcc2 = 30.0;
+        robot.movej(pos2, fTargetVel2, fTargetAcc2);
+        
         break;
-
- 
       }
       case '2':
       {
-        POSITION_EX tTargetPos;
-        float pos[6] = {0.,0.,30.,0.,30.,0.};
-        memcpy(tTargetPos._posj._pos, pos, sizeof(float)*6);
-        tTargetPos._pos_type = POSJ;
-        float fTargetVel[NUMBER_OF_JOINT] = {30., 30., 30., 30., 30., 30.};
-        float fTargetAcc[NUMBER_OF_JOINT] = {30., 30., 30., 30., 30., 30.};
-        
-        if(true != robot.movej(&tTargetPos, fTargetVel, fTargetAcc, 0.f, 0, MOVE_MODE_ABSOLUTE, 0.f, 0 )) {
-          std::cout << "Cannot set movej " << std::endl;
-          return 1;
-        }
-        std::cout << "Set movej done" <<std::endl;
+        // DRCF_VERSION 3 
+        // POSITION_EX tTargetPos;
+        // float pos[6] = {400., 500., 800., 0., 180.,0.};
+        // memcpy(tTargetPos._posx._pos, pos, sizeof(float)*6);
+        // tTargetPos._pos_type = POSX;
+        // float fTargetVel[2] = {30,30};
+        // float fTargetAcc[2] = {30,30};
+        // if(true != robot.movel(&tTargetPos, fTargetVel, fTargetAcc)) {
+        //   std::cout << "Cannot set movel1 " << std::endl;
+        //   return 1;
+        // }
+        // std::cout << "movel 1 " << std::endl;
+
+        // float pos1[6] = {400., 500., 500., 0., 180.,0.};
+        // memcpy(tTargetPos._posx._pos, pos1, sizeof(float)*6);
+        // if(true != robot.movel(&tTargetPos, fTargetVel, fTargetAcc)) {
+        //   std::cout << "Cannot set movel2 " << std::endl;
+        //   return 1;
+        // }
+        // std::cout << "movel2 " << std::endl;
+
+        // float pos2[6] = {400., 500., 800., 0., 180.,0.};
+        // memcpy(tTargetPos._posx._pos, pos2, sizeof(float)*6);
+        // if(true != robot.movel(&tTargetPos, fTargetVel, fTargetAcc)) {
+        //   std::cout << "Cannot set movel3 " << std::endl;
+        //   return 1;
+        // }
+        // std::cout << "movel3 " << std::endl;
         break;
       }
       case '3':
       {
-        POSITION_EX tTargetPos;
-        float pos[6] = {400., 500., 800., 0., 180.,0.};
-        memcpy(tTargetPos._posx._pos, pos, sizeof(float)*6);
-        tTargetPos._pos_type = POSX;
-        float fTargetVel[2] = {30,30};
-        float fTargetAcc[2] = {30,30};
-        if(true != robot.movel(&tTargetPos, fTargetVel, fTargetAcc)) {
-          std::cout << "Cannot set movel1 " << std::endl;
-          return 1;
-        }
-        std::cout << "movel 1 " << std::endl;
 
-        float pos1[6] = {400., 500., 500., 0., 180.,0.};
-        memcpy(tTargetPos._posx._pos, pos1, sizeof(float)*6);
-        if(true != robot.movel(&tTargetPos, fTargetVel, fTargetAcc)) {
-          std::cout << "Cannot set movel2 " << std::endl;
-          return 1;
-        }
-        std::cout << "movel2 " << std::endl;
-
-        float pos2[6] = {400., 500., 800., 0., 180.,0.};
-        memcpy(tTargetPos._posx._pos, pos2, sizeof(float)*6);
-        if(true != robot.movel(&tTargetPos, fTargetVel, fTargetAcc)) {
-          std::cout << "Cannot set movel3 " << std::endl;
-          return 1;
-        }
-        std::cout << "movel3 " << std::endl;
-        // float pos2[6] = {30., 30., 30., 0., 0.,0.};
-        // memcpy(tTargetPos._posx._pos, pos2, sizeof(float)*6);
-        // robot.movel(&tTargetPos, fTargetVel, fTargetAcc);
         break;
       }
       case '4':
       {
-        POSITION_EX tTargetPos;
-        float pos[6] = {0.,0.,50.,0.,50.,0.};
-        memcpy(tTargetPos._posj._pos, pos, sizeof(float)*6);
-        tTargetPos._pos_type = POSJ;
-        float fTargetVel[NUMBER_OF_JOINT] = {10., 10., 10., 10., 10., 10.};
-        float fTargetAcc[NUMBER_OF_JOINT] = {10., 10., 10., 10., 10., 10.};
-        
-        if(true != robot.amovej(&tTargetPos, fTargetVel, fTargetAcc, 0.f, 0, MOVE_MODE_ABSOLUTE, 0.f, 0 )) {
-          std::cout << "Cannot set movej " << std::endl;
-          break;
-        }
-         
-        std::cout << "Set movej done" <<std::endl;
-        robot.mwait();
-        std::string e = "QUERY STATE  : " + to_str(robot.query_operating_state());
-        std::cout <<  e << std::endl;
 
         break;
       }
       case '5':
       {
-        POSITION_EX tTargetPos;
-        float pos[6] = {0.,0.,0.,0.,0.,0.};
-        memcpy(tTargetPos._posj._pos, pos, sizeof(float)*6);
-        tTargetPos._pos_type = POSJ;
-        float fTargetVel[NUMBER_OF_JOINT] = {10., 10., 10., 10., 10., 10.};
-        float fTargetAcc[NUMBER_OF_JOINT] = {10., 10., 10., 10., 10., 10.};
-        
-        if(true != robot.amovej(&tTargetPos, fTargetVel, fTargetAcc, 0.f, 0, MOVE_MODE_ABSOLUTE, 0.f, 0 )) {
-          std::cout << "Cannot set movej " << std::endl;
-          break;
-        }
-        std::cout << "Set movej done" <<std::endl;
-        std::cout << "1111" <<std::endl;
-        robot.mwait();
-        std::string e = "QUERY STATE  : " + to_str(robot.query_operating_state());
-        std::cout <<  e << std::endl;
-        std::cout << "3333" <<std::endl;
+
         break;
       }
       case '6':
       {
           //robot.MoveStop(STOP_TYPE_QUICK_STO);
-          robot.SetRobotControl(CONTROL_RECOVERY_SAFE_OFF);
+          // robot.SetRobotControl(CONTROL_RECOVERY_SAFE_OFF);
           //robot.SetRobotControl(CONTROL_SERVO_ON);
           //if(true!=robot.SetRobotControl(CONTROL_SERVO_ON)){
         //  std::cout << "SetRobotControl(Servo On) failure " << std::endl;
@@ -355,28 +270,19 @@ int main() {
         //std::cout << "SetRobotControl(Servo On) done " << std::endl;
           //robot.set_robot_mode(ROBOT_MODE_MANUAL);
           //robot.SetRobotControl(CONTROL_SERVO_ON);
+          break;
       }
       case 'a':
       {
           
           robot.set_robot_mode(ROBOT_MODE_MANUAL);
           robot.SetRobotControl(CONTROL_SERVO_ON);
+          break;
       }
       case '7':
       {
-          POSITION_EX tTargetPos;
 
-          float posj[6] = { 180., 0., 90., 0., 90., 0. };
-          memcpy(tTargetPos._posj._pos, posj, sizeof(float) * 6);
 
-          float fTargetVel[NUMBER_OF_JOINT] = { 30., 30., 30., 30., 30., 30. };
-          float fTargetAcc[NUMBER_OF_JOINT] = { 30., 30., 30., 30., 30., 30. };
-          robot.amovej(&tTargetPos, fTargetVel, fTargetAcc, 0.0);
-          std::cout << "after amovej/" << std::endl;
-          //robot.MoveStop(STOP_TYPE_EMERGENCY);
-          robot.stop(STOP_TYPE_EMERGENCY);
-          
-        //std::string e = "QUERY STATE  : " + to_str(robot.query_operating_state());
         //std::cout <<  e << std::endl;
         //std::cout << "3333" <<std::endl;
       }
